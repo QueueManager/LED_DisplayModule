673c2599b5eaf6dd8621172d070202aa5e7aa14a
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-11-18 19:51:15 -0200

    Update slave.c

--- src/slave.c
+++ src/slave.c
@@ -24,30 +24,15 @@
 
 #include <xc.h>
 
-#define ID              0x00 // 0x0F slave2 | 0x00 slave1
+#define ID              0x01 // 0x01 slave2 | 0x00 slave1
 #define COUNTER_MASK    0b10100000
 #define DATA_OUT_MASK1  0b00011110
 #define DATA_OUT_MASK2  0b11100001
 #define DATA_IN_MASK1   0b10000000
 #define DATA_IN_MASK2   0b11100000
-#define SYNC            0x00
-#define RECEIVE         0x01
-#define WAIT            0x02
-#define SYNC_TIME       254
-#define RECEIVE_TIME    239
-#define WAIT_TIME       54
 #define inverter(data) ((data&0x02)<<3)|((data&0x04)<<1)|((data&0x08)>>1)|((data&0x10)>>3)
 
 //----------GLOBAL_VARIABLES---------
-/*char counter[6] = 
-{
-    0b01011110,
-    0b01011101,
-    0b01011011,
-    0b01010111,
-    0b01001111,
-    0b00011111
-};*/
 char counter[6] = 
 {   
     0b00011111,
@@ -59,7 +44,6 @@ char counter[6] =
 };
 char dataReceived[6] = {0x00};
 char dataOut[6] = {0x08, 0x08, 0x08, 0x08, 0x08, 0x08};
-char status = SYNC;
 char receiveCounter = 0x00;
 
 //-------------ROUTINES--------------
@@ -73,86 +57,25 @@ char getDataIn() {
 }
 
 void receiveData() {
-    if (receiveCounter >= 7) {
-        INTCONbits.INTE = 1;
-        return;
+    INTCONbits.INTE = 0;
+    INTCONbits.INTF = 0;
+    
+    if (receiveCounter >= 6) {
+        receiveCounter = 0;
     }
     
-    if (receiveCounter != 0) {
-        dataOut[receiveCounter-1] = /*0x07;*/getDataIn();
-        receiveCounter++;
-    }
-    else {
-        if (getDataIn() != ID) {
-            return;
-        }
-        else {
-            receiveCounter++;
-        }
-    }
-    
-    TMR0 = RECEIVE_TIME;
-    INTCONbits.T0IE = 1;
-}
-
-void updateDisplay() {
-    if (getDataIn() != ID)
-        return;
-    
-    //for (int i = 0; i < 6; ++i) {
-        //dataOut[i] = dataReceived[i];
-    //}
+    dataOut[receiveCounter] = getDataIn();
+    receiveCounter++;
     
     INTCONbits.INTE = 1;
 }
 
-//void inverter(char data) {
-//    asm("BCF data, 4");
-//_asm
-  //      BTFSS   data, 1
-   //     BSF     data, 4 
-//_endasm
-//}
-
 //------------INTERRUPTS-------------
-void startSyncDelay() {
-    INTCONbits.INTE = 0;
-    INTCONbits.INTF = 0;
-    INTCONbits.T0IE = 0;
-    INTCONbits.T0IF = 0;
-    
-    receiveCounter = 0x00;
-    status = SYNC;
-    
-    TMR0 = SYNC_TIME;
-    INTCONbits.T0IE = 1;
-}
-
-void handleTimeOut() {
-    INTCONbits.T0IE = 0;
-    INTCONbits.T0IF = 0;
-    
-    if (status == SYNC || status == WAIT) {
-        //receive handler
-        status = RECEIVE;
-        if (PORTAbits.RA5)
-            updateDisplay();
-        else
-            receiveData();
-    }
-    else if (status == RECEIVE) {
-        //wait handler
-        status = WAIT;
-        TMR0 = WAIT_TIME;
-        INTCONbits.T0IE = 1;
-    }
-}
 
 void interrupt int_handler() {
-    if (INTCONbits.INTF)
-        startSyncDelay();
-    else if (INTCONbits.T0IF)
-        handleTimeOut();
+    if (INTCONbits.INTF && (PORTAbits.RA5 == ID)) {
+        receiveData();
+    }
 }
 
 void delay()
